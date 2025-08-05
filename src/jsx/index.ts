9bed58326871b2eb55bc12c8c99715b681ca0550
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-06 00:44:52 +0900

    JSXElementの宣言でもpropsはJSX.IntrinsicElementsを参照する

--- src/jsx/index.ts
+++ src/jsx/index.ts
@@ -1,7 +1,7 @@
 import type { ComponentFunction } from "./component";
 
 export type JSXNode = JSXElement<keyof HTMLElementTagNameMap> | JSXComponent<any> | string;
-export type JSXElement<T extends keyof HTMLElementTagNameMap> = [T, {[key: string]: unknown}, ...JSXNode[]];
+export type JSXElement<T extends keyof HTMLElementTagNameMap> = [T, JSX.IntrinsicElements[T], ...JSXNode[]];
 export type JSXComponent<T extends object> = [ComponentFunction<T>, T, ...JSXNode[]];
 
 export type { Component } from "./component";
