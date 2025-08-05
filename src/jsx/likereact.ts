d7078955e25417cd38c3a63125bfe5245902bf4e
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-05 19:42:31 +0900

    一応JSXLikeReactFnにpropsがnullの可能性を付け加える

--- src/jsx/likereact.ts
+++ src/jsx/likereact.ts
@@ -1,5 +1,5 @@
 import type { AnyElType, Component, JSX } from ".";
 
-type JSXLikeReactFn<T extends AnyElType> = (type: T, props: T extends Component<infer P> ? P : { [key: string]: unknown }, ...children: JSX[]) => JSX;
+type JSXLikeReactFn<T extends AnyElType> = (type: T, props: T extends Component<infer P> ? P : (null | { [key: string]: unknown }), ...children: JSX[]) => JSX;
 
 export const h: JSXLikeReactFn<AnyElType> = (...args) => args;
