21d6371d23528dbd0e0ebce575356f153fa533a0
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-05 19:14:23 +0900

    ReactライクJSX APIも実装

---
+++ src/jsx/likereact.ts
@@ -0,0 +1,5 @@
+import type { AnyElType, Component, JSX } from ".";
+
+type JSXLikeReactFn<T extends AnyElType> = (type: T, props: T extends Component<infer P> ? P : { [key: string]: unknown }, ...children: JSX[]) => JSX;
+
+export const h: JSXLikeReactFn<AnyElType> = (...args) => args;
