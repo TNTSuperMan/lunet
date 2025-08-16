19c2dbc863242fb2e4972e3d716c8a57a986e3a1
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-16 13:41:22 +0900

    bunupが型を認識するようhとrenderの型を明確にする

--- src/jsx/likereact.ts
+++ src/jsx/likereact.ts
@@ -2,12 +2,15 @@ import type { JSXNode } from ".";
 import type { Component } from "./component";
 import { fragment } from "./fragment";
 
-export const h = <T extends keyof HTMLElementTagNameMap | Component<any> | typeof fragment>(
+type JSXFactoryFunction = <T extends keyof HTMLElementTagNameMap | Component<any> | typeof fragment>(
     type: T,
     props: null | T extends Component<infer P> ? P : { [key: string]: unknown },
     ...children: JSXNode[]
-) => typeof type === "string" ?
-    [type, props ?? {}, ...children] :
-    type === fragment ?
-        fragment(props, ...children) :
-        type(props);
+) => JSXNode;
+
+export const h: JSXFactoryFunction = (type, props, ...children) =>
+    typeof type === "string" ?
+        [type, props ?? {}, ...children] :
+        type === fragment ?
+            fragment(props, ...children) :
+            type(props);
--- src/mount/index.ts
+++ src/mount/index.ts
@@ -2,7 +2,9 @@ import type { JSXNode } from "../jsx";
 import { renderNode } from "./dom";
 import { revokerMap } from "./revokerMap";
 
-export const render = (el: HTMLElement, jsx: JSXNode) => {
+type RenderFunction = (el: HTMLElement, jsx: JSXNode) => void;
+
+export const render: RenderFunction = (el, jsx) => {
     el.childNodes.forEach(e=>{
         revokerMap.get(e)?.();
         e.remove();
