5e018df116448dcfc4f6726dbdd8f602e57745ca
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-17 11:18:54 +0900

    component系型修正

--- src/mount/dom/index.ts
+++ src/mount/dom/index.ts
@@ -5,14 +5,14 @@ import { renderFragment } from "./fragment";
 import { renderText } from "./text";
 
 export type RenderedDOM<T extends JSXNode> = [
-    T extends string ? 0 : T extends JSXElement ? 1 : T extends JSXFragment ? 2 : T extends JSXComponent<object> ? 3 : never, // 0 種類
+    T extends string ? 0 : T extends JSXElement ? 1 : T extends JSXFragment ? 2 : T extends JSXComponent ? 3 : never, // 0 種類
     () => (JSXElement | string)[], // 1 差分比較用のフラットJSX出力関数
     (jsx: T) => void,              // 2 差分更新関数
     () => Node,                    // 3 初回・トラブル時にフル描画をする関数
     () => void,                    // 4 破棄関数
 ]
 
-export type UnknownRenderedDOM = RenderedDOM<string> | RenderedDOM<JSXElement> | RenderedDOM<JSXFragment> | RenderedDOM<JSXComponent<object>>;
+export type UnknownRenderedDOM = RenderedDOM<string> | RenderedDOM<JSXElement> | RenderedDOM<JSXFragment> | RenderedDOM<JSXComponent>;
 
 export const renderNode = (jsx: JSXNode): UnknownRenderedDOM => {
     if(typeof jsx === "string"){
