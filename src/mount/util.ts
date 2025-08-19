1fbb89bf8c83841c53b8e88ea751c6eaea27ef2e
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-19 18:28:40 +0900

    isCompatibleNodeの内容を単純化

--- src/mount/util.ts
+++ src/mount/util.ts
@@ -1,12 +1,6 @@
 import type { JSXNode } from "../jsx";
 
-export const isCompatibleNode = (before: JSXNode, after: JSXNode): boolean => {
-    if(typeof before === "string" || typeof after === "string"){
-        return typeof before === typeof after;
-    }else{
-        if(before[0] !== after[0]) return false;
-        if("key" in before[1] && "key" in after[1])
-            return before[1].key === after[1].key;
-        return true;
-    }
-}
+export const isCompatibleNode = (before: JSXNode, after: JSXNode): boolean =>
+    typeof before === "string" || typeof after === "string" ?
+        typeof before === typeof after :
+        before[0] === after[0] && before[1].key === after[1].key;
