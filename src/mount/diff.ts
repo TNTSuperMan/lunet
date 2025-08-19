2e3da401be8640da36f0ead2e75f89231ce4228b
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-19 19:00:14 +0900

    差分検出コードも

--- src/mount/diff.ts
+++ src/mount/diff.ts
@@ -4,3 +4,31 @@ export const isCompatibleNode = (before: JSXNode, after: JSXNode): boolean =>
     typeof before === "string" || typeof after === "string" ?
         typeof before === typeof after :
         before[0] === after[0] && before[1].key === after[1].key;
+
+export type Patch =
+    | [0, number, JSXNode] // 更新
+    | [1, number, JSXNode] // 挿入
+    | [2, number] // 削除
+
+export const diff = (
+    before_nodes: JSXNode[],
+    after_nodes: JSXNode[],
+): Patch[] => {
+    const patches: Patch[] = [];
+    const max = Math.max(before_nodes.length, after_nodes.length);
+
+    for(let i = 0;i < max;i++){
+        const before = before_nodes[i];
+        const after = after_nodes[i];
+        
+        if(!before && after){
+            patches.push([1, i, after]);
+        }else if(before && !after){
+            patches.push([2, i]);
+        }else if(before && after && !isCompatibleNode(before, after)){
+            patches.push([0, i, after]);
+        }
+    }
+
+    return patches;
+}
