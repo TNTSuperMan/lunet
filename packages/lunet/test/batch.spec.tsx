c73189652b727f0adc00879121a14fe74ea101c9
TNTSuperMan <tnt-super-man@outlook.com>
2025-10-05 17:16:24 +0900

    バッチテスト

---
+++ packages/lunet/test/batch.spec.tsx
@@ -0,0 +1,37 @@
+import { expect, test } from "bun:test";
+import { createComponent, setBatch, h, fragment, render } from "../src";
+
+test("Check batch", () => {
+    let batching_flag = false;
+    const batch_info: number[] = [];
+    setBatch(cb => {
+        batch_info.unshift(0);
+        batching_flag = true;
+        cb();
+        batching_flag = false;
+    });
+
+    const Child = createComponent<{ a: string, b: string }>((render, init) => {
+        render("");
+        return {
+            set a(_: string) { expect(batching_flag).toBeTrue(); batch_info[0]++; },
+            set b(_: string) { expect(batching_flag).toBeTrue(); batch_info[0]++; }
+        }
+    });
+
+    const Parent = createComponent((render, init) => {
+        const render_ = () => render(<>
+            <button $click={render_}></button>
+            <Child a="a" b="b" />
+        </>);
+        render_();
+        return init;
+    });
+
+    render(document.body, <Parent/>);
+
+    document.querySelector("button")?.click();
+    document.querySelector("button")?.click();
+
+    expect(batch_info).toEqual([2, 2]);
+});
