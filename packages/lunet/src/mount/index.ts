8be4b604588342ac1bb0ffb90066e2b476c94d87
TNTSuperMan <tnt-super-man@outlook.com>
2025-10-06 15:03:21 +0900

    配列の欠陥で奇数番目の要素が残るバグを修正

--- packages/lunet/src/mount/index.ts
+++ packages/lunet/src/mount/index.ts
@@ -5,7 +5,7 @@ import { revokerMap } from "./revokerMap";
 type RenderFunction = (el: HTMLElement, jsx: JSXNode) => void;
 
 export const render: RenderFunction = (el, jsx) => {
-    el.childNodes.forEach(e=>{
+    Array.from(el.childNodes).forEach(e=>{
         revokerMap.get(e)?.();
         e.remove();
     });
