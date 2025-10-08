fb8b8bf91ce8f688c3714758bc740983dcde8e20
TNTSuperMan <tnt-super-man@outlook.com>
2025-10-08 13:36:57 +0900

    renderTextをちょっと最適化

--- packages/lunet/src/mount/dom/text.ts
+++ packages/lunet/src/mount/dom/text.ts
@@ -1,14 +1,13 @@
 import type { RenderedDOM } from ".";
 
-export const renderText = (jsx: string): RenderedDOM<string> => {
-    let currentText = jsx;
+export const renderText = (text: string): RenderedDOM<string> => {
     let node: Text | null;
 
     return {
         // type: 0,
         // flat: () => [currentText],
-        update(jsx){ currentText !== jsx && (node!.textContent = currentText = jsx) },
-        render: () => node = new Text(currentText),
+        update(t){ text !== t && (node!.textContent = text = t) },
+        render: () => node = new Text(text),
         revoke(){ node!.remove() },
         after(n) { node!.parentNode!.insertBefore(n, node!.nextSibling) },
     }
