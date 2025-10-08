5f458783f4720870de866c49ea1a774311fb5cf5
TNTSuperMan <tnt-super-man@outlook.com>
2025-10-08 13:01:03 +0900

    textのNodeを毎回生成する

--- packages/lunet/src/mount/dom/text.ts
+++ packages/lunet/src/mount/dom/text.ts
@@ -2,14 +2,14 @@ import type { RenderedDOM } from ".";
 
 export const renderText = (jsx: string): RenderedDOM<string> => {
     let currentText = jsx;
-    const node = new Text(currentText);
+    let node: Text | null;
 
     return {
         type: 0,
         flat: () => [currentText],
-        update(jsx){ currentText !== jsx && (node.textContent = currentText = jsx) },
-        render: () => node,
-        revoke(){ node.remove() },
-        after(n) { node.parentNode!.insertBefore(n, node.nextSibling) },
+        update(jsx){ currentText !== jsx && (node!.textContent = currentText = jsx) },
+        render: () => node = new Text(currentText),
+        revoke(){ node!.remove() },
+        after(n) { node!.parentNode!.insertBefore(n, node!.nextSibling) },
     }
 }
