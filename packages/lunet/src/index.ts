e2aa291dbe91bdc7b87ccb938bb0dac0f6c97f4b
TNTSuperMan <111977067+TNTSuperMan@users.noreply.github.com>
2026-01-16 01:24:48 +0900

    ts-ignoreで強行突破☆

--- packages/lunet/src/index.ts
+++ packages/lunet/src/index.ts
@@ -1,5 +1,4 @@
 import "./jsx/type/jsx";
-import type { createFragment as createFragment$T, updateFragment as updateFragment$T, revokeFragment as revokeFragment$T } from "./render";
 import type { Config } from "./render/config";
 
 export * from "./jsx";
@@ -8,19 +7,20 @@ export const createRoot = (el: HTMLElement, __CONFIG: Config): {
     render(jsx: JSX.Element): void;
     unmount(): void;
 } => {
-    //@ts-ignore
-    var createFragment: typeof createFragment$T = null, updateFragment: typeof updateFragment$T = null, revokeFragment: typeof revokeFragment$T = null;
     
     //@INJECT
 
+    //@ts-ignore
     const [node, dom] = createFragment([null, {}]);
     el.append(dom);
 
     return {
         render(jsx) {
+            //@ts-ignore
             updateFragment(node, [null, {}, jsx]);
         },
         unmount() {
+            //@ts-ignore
             revokeFragment(node);
         },
     }
